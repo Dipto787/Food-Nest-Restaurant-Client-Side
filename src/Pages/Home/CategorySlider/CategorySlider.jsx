679bf58c1a5ee0ed9658c2329c3335a9b6617d07
Dipto Roy <diptoray292@gmail.com>
2025-01-29 18:00:09 +0600

    Project-Setup, Create a Navbar,Banner,Slider Section

---
+++ src/Pages/Home/CategorySlider/CategorySlider.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import { Pagination } from 'swiper/modules';   
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';  
+import img1 from '../../../assets/home/slide1.jpg';
+import img2 from '../../../assets/home/slide2.jpg';
+import img3 from '../../../assets/home/slide3.jpg';
+import img4 from '../../../assets/home/slide4.jpg';
+import img5 from '../../../assets/home/slide5.jpg' 
+import SectionTitle from '../../../Components/Shared/SectionTitle/SectionTitle';
+const CategorySlider = () => {
+    return (
+        <div className='mb-4'>
+            <SectionTitle subHeading={'From 11:00am to 10:00pm'} heading={'ORDER NOW'}></SectionTitle>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide className=''>
+                    <img className='' src={img1} alt="" />
+                    <h1 className='text-4xl text-center -mt-16 text-white   '>Salads</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='' src={img2} alt="" />
+                    <h1 className='text-4xl text-center -mt-16 text-white   '>Soups</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='' src={img3} alt="" />
+                    <h1 className='text-4xl text-center -mt-16 text-white   '>pizzas</h1> 
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='' src={img4} alt="" />
+                    <h1 className='text-4xl text-center -mt-16 text-white   '>desserts</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='' src={img5} alt="" />
+                    <h1 className='text-4xl text-center -mt-16 text-white   '>Salads</h1>
+                </SwiperSlide>
+            
+            </Swiper>
+        </div>
+    );
+};
+
+export default CategorySlider;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import CategorySlider from "../CategorySlider/CategorySlider";
+import Slider from "../Slider/Slider";
+
+ 
+const Home = () => {
+    return (
+        <div>
+            This is the home page
+        <Slider></Slider>
+        <CategorySlider></CategorySlider>
+        </div>
+    );
+};
+
+export default Home;
